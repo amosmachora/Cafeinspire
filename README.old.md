b71c87c5ac177cffcfb0a7071edb6556671642e6
AmohPrince <Amohprincethedoctor@gmail.com>
2022-02-03 10:09:17 +0300

    Added more files

---
+++ README.old.md
@@ -0,0 +1,6 @@
+# Cafeinspire
+
+This is a repo for a modern website in React with complete ui/ux and modern responsiveness.
+Feel free to clone and view it on the link
+
+Thanks😉
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import react from "react";
-import Reactdom from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
+import './index.css';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
+
